9320d5e32fdd7bbf58ba32c2adcb57259fc20de0
Patrick Yeon <patrickyeon@gmail.com>
2012-02-05 01:23:39 -0500

    Working code. Go me.

---
+++ destruct.py
@@ -0,0 +1,86 @@
+import struct as s
+
+def unpack(fmt, buff):
+    order = '@'
+    if fmt[0] in '@=<>!':
+        order, fmt = fmt[0], fmt[1:]
+    ret = []
+    start = 0
+    while start < len(fmt):
+        op = fmt.find('(', start)
+        cl = fmt.find(')', start)
+        if op > -1 and op < cl:
+            subfmt = order + fmt[start:op]
+            start = op + 1
+            if s.calcsize(subfmt) > 0:
+                ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
+                #ret.append(str(buff.read(s.calcsize(subfmt))))
+            consumed, parsed = unpack(order + fmt[start:], buff)
+            ret.append(parsed)
+            start += consumed
+        elif cl > -1:
+            subfmt = order + fmt[start:cl]
+            start = cl + 1
+            ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
+            #ret.append(str(buff.read(s.calcsize(subfmt))))
+            return start, ret
+        else:
+            subfmt = order + fmt[start:]
+            if s.calcsize(subfmt) > 0:
+                ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
+                #ret.append(str(buff.read(s.calcsize(subfmt))))
+            start = len(fmt)
+    return ret
+
+class buf(object):
+    def _nie(self, foo=None):
+        raise NotImplementedError
+    pos = property(_nie, _nie)
+    
+    def read(self, size):
+        raise NotImplementedError
+
+class strbuf(buf):
+    def __init__(self, string):
+        self._buf = buffer(string)
+        self._pos = 0
+
+    def __len__(self):
+        """ Number of bytes left in the buffer """
+        return len(self._buf) - self.pos
+
+    def seek_to(self, to):
+        self._pos = to
+    def tell(self):
+        return self._pos
+    pos = property(tell, seek_to)
+
+    def read(self, size):
+        offset = self.pos
+        self.pos = min(offset + size, len(self._buf))
+        return buffer(self._buf, offset, size)
+
+class filebuf(buf):
+    def __init__(self, file_or_filename):
+        """ Only actual files please. Need a static end, and seekable """
+        if isinstance(file_or_filename, file):
+            self._f = file_or_filename
+        else:
+            self._f = open(file_or_filename)
+        # I hope I'm not damaging anything by doing this
+        self._f.seek(0, 2)
+        self._end = self._f.tell()
+        self._f.seek(0)
+        self._pos = 0
+
+    def __len__(self):
+        return self._end - self._pos
+
+    def seek_to(self, to):
+        self._f.seek(to, 1)
+    def tell(self):
+        return self._f.tell()
+    pos = property(tell, seek_to)
+
+    def read(self, size):
+        return buffer(self._f.read(size))
