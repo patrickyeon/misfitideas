0b70754965453436462ee4adf841384a464863e3
Patrick Yeon <patrickyeon@gmail.com>
2012-02-05 02:07:45 -0500

    Mostly comments, docstrings

--- destruct.py
+++ destruct.py
@@ -1,30 +1,45 @@
 import struct as s
 
 def unpack(fmt, buff):
+    ''' Extended unpacking, buff needs to be a destruct.buf subclass
+
+        Format string is a strict superset of the struct format strings. Use
+        parens '(' and ')' to delimit lists (arrays).'''
     order = '@'
     if fmt[0] in '@=<>!':
+        # going to want to distribute the byte order to sub-strings
         order, fmt = fmt[0], fmt[1:]
     ret = []
     start = 0
     while start < len(fmt):
+        # any special chars?
         op = fmt.find('(', start)
         cl = fmt.find(')', start)
         if op > -1 and op < cl:
+            # unpack until opening paren, then recurse to handle content inside
+            # the parens
             subfmt = order + fmt[start:op]
-            start = op + 1
+            start = op + 1 # nobody else needs to see the open paren
             if s.calcsize(subfmt) > 0:
+                # ret.extend() to flatten the tuple returned by s.unpack()
                 ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
+                # v-- if ever need to see which substrings are going where
                 #ret.append(str(buff.read(s.calcsize(subfmt))))
             consumed, parsed = unpack(order + fmt[start:], buff)
             ret.append(parsed)
+            # make sure to account for chars handled
             start += consumed
         elif cl > -1:
+            # unpack until close paren, then bump it up the call stack
             subfmt = order + fmt[start:cl]
             start = cl + 1
-            ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
-            #ret.append(str(buff.read(s.calcsize(subfmt))))
+            if s.calcsize(subfmt) > 0:
+                ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
+                # v-- to see substrings
+                #ret.append(str(buff.read(s.calcsize(subfmt))))
             return start, ret
         else:
+            # unpack to the end of the format string
             subfmt = order + fmt[start:]
             if s.calcsize(subfmt) > 0:
                 ret.extend(s.unpack(subfmt, buff.read(s.calcsize(subfmt))))
@@ -33,11 +48,18 @@ def unpack(fmt, buff):
     return ret
 
 class buf(object):
+    ''' Buffers that work for me. No relation to the native buffer.
+
+        buf.pos is the position in the byte stream.'''
     def _nie(self, foo=None):
         raise NotImplementedError
     pos = property(_nie, _nie)
     
     def read(self, size):
+        ''' returns a [native] buffer of (at least?) size bytes.'''
+        raise NotImplementedError
+    def __len__(self):
+        ''' returns number of bytes from current position to end of stream.'''
         raise NotImplementedError
 
 class strbuf(buf):
@@ -46,7 +68,6 @@ class strbuf(buf):
         self._pos = 0
 
     def __len__(self):
-        """ Number of bytes left in the buffer """
         return len(self._buf) - self.pos
 
     def seek_to(self, to):
@@ -64,8 +85,11 @@ class filebuf(buf):
     def __init__(self, file_or_filename):
         """ Only actual files please. Need a static end, and seekable """
         if isinstance(file_or_filename, file):
+            # TODO allow user to pass pre-seeked file, use position at
+            # construction as base offset?
             self._f = file_or_filename
         else:
+            # TODO open specifically as binary?
             self._f = open(file_or_filename)
         # I hope I'm not damaging anything by doing this
         self._f.seek(0, 2)
@@ -83,4 +107,6 @@ class filebuf(buf):
     pos = property(tell, seek_to)
 
     def read(self, size):
+        # TODO something to make sure size bytes are read?
+        # TODO buffer map into the file, not create a new string?
         return buffer(self._f.read(size))
